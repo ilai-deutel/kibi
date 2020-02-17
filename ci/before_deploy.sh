215b048c8c22bbb39e7e6692395d09b7641075fc
Ilaï Deutel <ilai-deutel@users.noreply.github.com>
2020-02-16 21:48:25 -0800

    Check Cargo.toml version before deploying

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -2,10 +2,18 @@
 
 set -euxo pipefail
 
+# Obtain the version from Cargo.toml, prepend "v".
+version=$(sed -n 's/version *= *"\(.*\)"/v\1/p' Cargo.toml)
+
+if [ "$version" != "$TRAVIS_TAG" ]; then
+  echo "Version in Cargo.toml ($version) and tag ($TRAVIS_TAG) should be equal." >&2
+  exit 1
+fi
+
 cargo build --bins --target "$TARGET" --release --verbose
 
 tempdir=$(mktemp -d 2>/dev/null || mktemp -d -t tmp)
-package_name="kibi-$TRAVIS_TAG-$TARGET"
+package_name="kibi-$version-$TARGET"
 
 mkdir "$tempdir/$package_name"
 
