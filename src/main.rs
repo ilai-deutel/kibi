28b1c8294a5924684e90d3c2a09fd0f6c706b54a
Ilaï Deutel <ilai-deutel@users.noreply.github.com>
2020-03-29 01:33:59 -0700

    Reorganize error management; simplify main()

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 //! # Kibi
 
-use kibi::{ansi_escape::CLEAR_SCREEN, ansi_escape::MOVE_CURSOR_TO_START, Config, Editor, Error};
+use kibi::{Config, Editor, Error};
 
 /// Load the configuration, initialize the editor and run the program, optionally opening a file if
 /// an argument is given.
@@ -8,14 +8,10 @@ use kibi::{ansi_escape::CLEAR_SCREEN, ansi_escape::MOVE_CURSOR_TO_START, Config,
 /// # Errors
 ///
 /// Any error that occur during the execution of the program will be returned by this function.
-fn run(mut args: std::env::Args) -> Result<(), Error> {
-    if args.len() > 2 {
-        return Err(Error::TooManyArguments(args.len() - 1));
+fn main() -> Result<(), Error> {
+    let mut args = std::env::args();
+    match (args.nth(1), args.nth(2)) {
+        (_, Some(_)) => Err(Error::TooManyArguments(args.len() - 1)),
+        (file_name, None) => Editor::new(Config::load()?)?.run(file_name),
     }
-    Editor::new(Config::load()?)?.run(args.nth(1))
-}
-
-fn main() {
-    let err_str = run(std::env::args()).err().map_or("".into(), |e| e.to_string() + "\n");
-    print!("{}{}{}", CLEAR_SCREEN, MOVE_CURSOR_TO_START, err_str);
 }
