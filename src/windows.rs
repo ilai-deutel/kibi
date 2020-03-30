45941f97e6e493869e6b7fb088d8f8b65de8ced8
Ilaï Deutel <ilai-deutel@users.noreply.github.com>
2020-03-30 13:37:10 -0700

    unix::get_window_size should return error if ioctl failed

--- src/windows.rs
+++ src/windows.rs
@@ -15,6 +15,7 @@ pub(crate) type TermMode = u32;
 /// Return configuration directories for Windows systems
 pub(crate) fn conf_dirs() -> [Option<String>; 1] { [std::env::var("APPDATA").ok()] }
 
+/// Return the current window size as (rows, columns).
 pub(crate) fn get_window_size() -> Result<(usize, usize), Error> {
     let w_rect = console::screen_buffer_info(HandleRef::stdout())?.window_rect();
     Ok(((w_rect.Bottom - w_rect.Top + 1) as usize, (w_rect.Right - w_rect.Left + 1) as usize))
