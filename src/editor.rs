4296dc942c542a1f38b76e218cc62da2cdbcd439
Jorge Reyes <reyestrevjorge@gmail.com>
2021-09-21 23:37:16 -0500

    :sparkles: Add remove_current_line functionality
    Added the 'remove_current_line' functionality, executable with 'Ctrl + R'

--- src/editor.rs
+++ src/editor.rs
@@ -16,6 +16,7 @@ const FIND: u8 = ctrl_key(b'F');
 const GOTO: u8 = ctrl_key(b'G');
 const DUPLICATE: u8 = ctrl_key(b'D');
 const EXECUTE: u8 = ctrl_key(b'E');
+const REMOVE_LINE: u8 = ctrl_key(b'R');
 const BACKSPACE: u8 = 127;
 
 const HELP_MESSAGE: &str =
@@ -387,6 +388,20 @@ impl Editor {
         }
     }
 
+    fn delete_current_row(&mut self) {
+        if self.cursor.y < self.rows.len() && self.cursor.y > 0 {
+            let row = &mut self.rows[self.cursor.y];
+            row.chars.clear();
+            self.update_row(self.cursor.y, false);
+            self.cursor.x = 0;
+            self.dirty = if self.is_empty() { self.file_name.is_some() } else { true };
+        } else if self.cursor.y == self.rows.len() {
+            self.move_cursor(&AKey::Left);
+        }
+        // The lines numbers has changed 
+        self.update_screen_cols()
+    }
+
     fn duplicate_current_row(&mut self) {
         if let Some(row) = self.current_row() {
             let new_row = Row::new(row.chars.clone());
@@ -590,6 +605,7 @@ impl Editor {
             Key::End => self.cursor.x = self.current_row().map_or(0, |row| row.chars.len()),
             Key::Char(b'\r') => self.insert_new_line(), // Enter
             Key::Char(BACKSPACE) | Key::Char(DELETE_BIS) => self.delete_char(), // Backspace or Ctrl + H
+            Key::Char(REMOVE_LINE) => self.delete_current_row(),
             Key::Delete => {
                 self.move_cursor(&AKey::Right);
                 self.delete_char()
