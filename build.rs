925ee890ff04c40c2462050ca7babce42787bbd5
Robert Grancsa <robert.grancsa2002@gmail.com>
2022-11-24 15:42:35 +0200

    Fixed clippy issues with build.rs
    Signed-off-by: Robert Grancsa <robert.grancsa2002@gmail.com>

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 use std::process::Command;
 
 fn main() {
-    let version = match Command::new("git").args(&["describe", "--tags", "--match=v*"]).output() {
+    let version = match Command::new("git").args(["describe", "--tags", "--match=v*"]).output() {
         Ok(output) if output.status.success() =>
             String::from_utf8_lossy(&output.stdout[1..]).replacen('-', ".r", 1).replace('-', "."),
         _ => env!("CARGO_PKG_VERSION").into(),
